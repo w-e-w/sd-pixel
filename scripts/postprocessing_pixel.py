bd3de8c263c8337ac2015752568bd1f969ab093d
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-08-12 22:15:48 +0900

    re-work UI

--- scripts/postprocessing_pixel.py
+++ scripts/postprocessing_pixel.py
@@ -1,20 +1,54 @@
 from PIL import Image
 from modules import scripts_postprocessing
 import gradio as gr
-from modules.ui_components import FormRow, FormGroup
 
-mode_dict = {"Nearest": Image.NEAREST,
-             "Bicubic": Image.BICUBIC,
-             "Bilinear": Image.BILINEAR,
-             "Hamming": Image.HAMMING,
-             "Lanczos": Image.LANCZOS}
+try:
+    if hasattr(scripts_postprocessing.ScriptPostprocessing, 'process_firstpass'):  # webui >= 1.7
+        from modules.ui_components import InputAccordion
+    else:
+        InputAccordion = None
+except ImportError:
+    InputAccordion = None
+
+
+class GoodInputAccordion:
+    def __init__(self, value, label, row=None, **kwargs):
+        self.value = value
+        self.label = label
+        self.kwargs = kwargs
+        self.row: gr.Row = row
+
+    def __enter__(self):
+        self.accordion = InputAccordion(self.value, label=self.label) if InputAccordion else gr.Accordion(self.label, open=False)
+        self.accordion.__enter__()
+
+        if self.row:
+            self.row = gr.Row()
+            self.row.__enter__()
+
+        return self.accordion if InputAccordion else gr.Checkbox(False, label='Enable')
+
+    def __exit__(self, *args):
+        if self.row:
+            self.row.__exit__()
+        self.accordion.__exit__(*args)
+
+
+mode_dict = {
+    "Nearest": Image.NEAREST,
+    "Bicubic": Image.BICUBIC,
+    "Bilinear": Image.BILINEAR,
+    "Hamming": Image.HAMMING,
+    "Lanczos": Image.LANCZOS
+}
 
 
 def downscale_image(img, scale, mode):
     width, height = img.size
-    img = img.resize(
-        (int(width / scale), int(height / scale)), mode_dict[str(mode)])
-    return img
+    return img.resize(
+        (int(width / scale), int(height / scale)),
+        mode_dict[str(mode)],
+    )
 
 
 def palette_limit(img, palette_size=16):
@@ -24,13 +58,12 @@ def palette_limit(img, palette_size=16):
 
 
 def rescale_image(img, original_size):
-    width, height = original_size
     # rescale the image
-    scaled_img = img.resize((width, height), Image.NEAREST)
+    scaled_img = img.resize(original_size, Image.NEAREST)
     return scaled_img
 
 
-def grayscalelimit(img, graylimit=155):
+def grayscale_limit(img, gray_limit=155):
     # Convert the image to grayscale
     img_gray = img.convert('L')
 
@@ -46,108 +79,71 @@ def grayscalelimit(img, graylimit=155):
         for y in range(img_gray.height):
             # If the grayscale pixel is less than or equal to 128,
             # set the corresponding pixel in the black and white image to black
-            if pixels_gray[x, y] <= graylimit:
+            if pixels_gray[x, y] <= gray_limit:
                 pixels_bw[x, y] = 0
 
     return img_bw
 
 
-class ScriptPostprocessingUpscale(scripts_postprocessing.ScriptPostprocessing):
+class PostprocessingPixel(scripts_postprocessing.ScriptPostprocessing):
     name = "pixel"
     order = 20000
-    model = None
 
     def ui(self):
-        with FormGroup():
-            # Pixelate and rescale
-            with FormRow():
-                # Enable or disable pixelation
-                pixelate_cb = gr.Checkbox(label="Pixelate", value=False)
+        with GoodInputAccordion(False, "Pixel") as enable:
+            with gr.Row():
+                # Pixelate and restore scale
+                with GoodInputAccordion(False, "Pixelate and Rescale", True) as enable_pixelate:
+                    with gr.Row():
+                        downscale = gr.Slider(label="Downscale", minimum=1, maximum=32, step=1, value=8)
+                        mode = gr.Dropdown(label="Mode", choices=list(mode_dict.keys()), value=list(mode_dict.keys())[0], multiselect=False)
+                        rescale = gr.Checkbox(label="Rescale (keep resolution)", value=False)
 
-                # Enable or disable rescaling
-                rescale = gr.Checkbox(label="Rescale", value=False)
+                # Color palette limit
+                with GoodInputAccordion(False, "Color Palette Limit", True) as enable_palette_limit:
+                    with gr.Row():
+                        palette_size = gr.Slider(label="Palette Size", minimum=0, maximum=256, step=1, value=1)
 
-            # Downscale
-            with FormRow(visible=False) as downscale_row:
-                # Adjust the downscaling factor
-                downscale = gr.Slider(label="Downscale", minimum=1, maximum=32, step=1, value=8)
-                # Select downscaling mode
-                mode = gr.Dropdown(label="Mode", choices=list(mode_dict.keys()), value=list(mode_dict.keys())[0], multiselect=False)
-
-            # Palette
-            with FormRow():
-                # Enable or disable palette limit
-                palette_limit_cb = gr.Checkbox(label="Color Palette Limit", value=False)
-            with FormRow(visible=False) as palette_row:
-                # Adjust the palette size
-                palette_size = gr.Slider(label="Palette Size", minimum=0, maximum=256, step=1, value=1)
-
-            # Graylimit
-            with FormRow():
-                # Enable or disable gray thresholding
-                gray_threshold_cb = gr.Checkbox(label="Gray Thresholding", value=False)
-                with FormRow(visible=False) as gray_threshold_row:
-                    # Adjust the graylimit value
+                # Gray limit
+                with GoodInputAccordion(False, "Gray Limit", True) as enable_gray_limit:
                     gray_threshold = gr.Slider(label="Threshold", minimum=0, maximum=255, step=1, value=0)
 
-                pixelate_cb.change(
-                    fn=lambda x: gr.update(visible=x),
-                    inputs=[pixelate_cb],
-                    outputs=[downscale_row],
-                )
-
-                palette_limit_cb.change(
-                    fn=lambda x: gr.update(visible=x),
-                    inputs=[palette_limit_cb],
-                    outputs=[palette_row],
-                )
-
-                gray_threshold_cb.change(
-                    fn=lambda x: gr.update(visible=x),
-                    inputs=[gray_threshold_cb],
-                    outputs=[gray_threshold_row],
-                )
-
             return {
-                "pixelate_cb": pixelate_cb,
-                "rescale": rescale,
-                "downscale": downscale,
-                "mode": mode,
-
-                "palette_limit_cb": palette_limit_cb,
-                "palette_size": palette_size,
-
-                "gray_limit_cb": gray_threshold_cb,
-                "graylimit": gray_threshold,
+                "enable": enable,
+                "enable_pixelate": enable_pixelate, "rescale": rescale, "downscale": downscale, "mode": mode,
+                "enable_palette_limit": enable_palette_limit, "palette_size": palette_size,
+                "enable_gray_limit": enable_gray_limit, "gray_limit": gray_threshold,
             }
 
-    def process(self, pp: scripts_postprocessing.PostprocessedImage, pixelate_cb, rescale, downscale, mode,
-                palette_limit_cb,
-                palette_size, gray_limit_cb, graylimit):
+    def process(
+            self, pp: scripts_postprocessing.PostprocessedImage, enable,
+            enable_pixelate, rescale, downscale, mode,
+            enable_palette_limit, palette_size,
+            enable_gray_limit, gray_limit,
+    ):
+        if not enable:
+            return
 
-        # if image is not RGBA, convert it to RGBA
-        if pp.image.mode != 'RGBA':
-            img = pp.image.convert('RGBA')
-        else:
-            img = pp.image
+        # convert the image to RGBA if it is not already
+        img = pp.image if pp.image.mode == 'RGBA' else pp.image.convert('RGBA')
 
         original_size = img.size
         applied_effects = ""
 
-        if pixelate_cb and downscale > 1:
+        if enable_pixelate and downscale > 1:
             img = downscale_image(img, downscale, mode)
             applied_effects += f"Downscale: {downscale}, Mode: {mode}, "
 
-        if palette_limit_cb and palette_size > 1:
+        if enable_palette_limit and palette_size > 1:
             img = palette_limit(img, palette_size)
             applied_effects += f"Color Palette Limit: {palette_size}, "
 
-        if gray_limit_cb and graylimit > 0:
-            img = grayscalelimit(img, graylimit)
-            applied_effects += f"Gray Limit: {graylimit}, "
+        if enable_gray_limit and gray_limit > 0:
+            img = grayscale_limit(img, gray_limit)
+            applied_effects += f"Gray Limit: {gray_limit}, "
 
         # Pass the original size and the image to the rescale_image function
-        if rescale and pixelate_cb:
+        if rescale and enable_pixelate:
             img = rescale_image(img, original_size)
             applied_effects += f"rescale, "
 
